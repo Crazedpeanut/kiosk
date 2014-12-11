c4934e53ccc0b88005a57fe3adb1d0d8de1585dc
John <john@johnkendall.net>
2014-12-11 14:23:27 +1100

    updates to kiosk client

--- kiosk_client_v2/kiosk_main.py
+++ kiosk_client_v2/kiosk_main.py
@@ -2,6 +2,7 @@ import kiosk_http as http
 import time, datetime
 import simplejson as json
 import read_from_hidraw as bcode_listen
+import socket
 
 HOST = "localhost"
 PORT = 80
@@ -16,10 +17,9 @@ DATA_FILE = "data.json"
 def get_kiosk_id():
 	return 1;
 
-def create_check_in(barcode):
-	timestamp = datetime.datetime.now()
+def create_check_in(barcode):	
 	kiosk = get_kiosk_id()
-	return '{"barcode": %s, "timestamp": %s, "kiosk": %s}\n' % (barcode, timestamp, kiosk)
+	return '{"barcode": %s, "kiosk": %s}\n' % (barcode, kiosk)
 
 def record_check_in(check_in):
 	try:
@@ -29,8 +29,11 @@ def record_check_in(check_in):
 		print(str(e))
 
 def create_heartbeat():
-	
-
+	timestamp = datetime.datetime.now()
+	kiosk = get_kiosk_id()
+	name = socket.gethostname()
+	ip = socket.gethostbyname(name)
+	return '{"timestamp": %s, "kiosk": %s, "name": %s, "ip": %s}' % (timestamp, kiosk, name, ip)
 
 #TODO
 def http_result_handler(result):
@@ -41,21 +44,16 @@ def bcode_handler(bcode):
 	record_check_in(check_in)
 
 def main():
-	kiosk_id = str(get_kiosk_id())
-	timestamp = str(datetime.datetime.now())
-	barcode = "900"
-
-	data = '{"time": "' +  timestamp + '", "station": "' +  kiosk_id + '", "barcode": "' + barcode + '"}'
-	print(data)
-	while(True):
-		http_result = http.http_request(HOST, PORT, METHOD, RESOURCE, json.loads(data))	
-		time.sleep(PAUSE_BETWEEN_REQUESTS)
-		http_result_handler(http_result)
-if __name__ == "__main__":
-	#main()	
-
+	
 	bcode_listen.start_listening(bcode_handler)
+	
+	while(True):
+		#http_result = http.http_request(HOST, PORT, METHOD, RESOURCE, json.loads(data))	
+		#http_result_handler(http_result)
+		print(create_heartbeat())
+		time.sleep(PAUSE_BETWEEN_REQUESTS)
+if __name__ == "__main__":
+	main()	
 
-	i = 1
-	while(True):	
-		i = i + 1
+	
+
